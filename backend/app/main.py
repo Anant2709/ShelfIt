7befef308272951437535b30785fbde59a302256
Anant Agrawal <anant04@terpmail.umd.edu>
2025-06-17 02:14:07 -0400

    sprint-0: Added front end CORS Changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from app.api import health
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Instantiate the web app
 app = FastAPI(
     title="ShelfIt",
     version="0.0.1",
     docs_url="/docs"
+)
+
+# configure CORS (Cross-Origin Resource Sharing) middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Register the health router under /api/health
